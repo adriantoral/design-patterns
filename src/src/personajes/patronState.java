3cab864b0b1f45516147fd9a90ed67389290f700
pauss12 <paulamendezsv@gmail.com>
2023-05-11 18:13:06 +0200

    Update patronState.java
    He añadido el comentario para la clase State, estados de vida del personaje

--- src/src/personajes/patronState.java
+++ src/src/personajes/patronState.java
@@ -1,6 +1,18 @@
 package personajes;
 
-public class patronState
-{
+/*
+ * Controlar el estado de los personajes durante el combate, de forma que los personajes vayan pasando de un estado a otro.
+ * El Personaje va a tener diferentes estados, entre (MUERTO, MALHERIDO, HERIDO, SALUDABLE, NULO)
+ * 
+ * - Muerto => Cuando se ha quedado sin vida o esta a punto (0-25)
+ * - Malherido => Cuando esta a punto de quedarse sin vida (25-50)
+ * - Herido => Cuando ha sido atacado (50-75)
+ * - Saludable => Cuando tiene toda la vida o la mayor parte (75-100)
+ * - Nulo => En cualquier otro caso (ERROR)
+ * */
+
+public class patronState{
+	
+	
 
 }
