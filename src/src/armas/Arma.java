cc43bb078a66f81159678f8241226cc17e28af2e
adriantoral <adrian.rubiboy11@gmail.com>
2023-05-11 19:12:44 +0200

    Añade un nombre a las Armas.

--- src/src/armas/Arma.java
+++ src/src/armas/Arma.java
@@ -7,22 +7,25 @@ public abstract class Arma
      * La utilidad es la unidad que mide si el arma es valido para atacar o no, si es mayor que 0 podra atacar, si es 0 no podra atacar.
      * La agilidad le aporta mayor agilidad al personaje, es decir, que le suma agilidad del personaje.
      * Si tiene el personaje 100 de agilidad, y el arma 15 de agilidad, el personaje en realidad tendra 115 de agilidad y atacara mas rapido.
+     * El nombre identifica el arma.
      */
-    int danio, utilidad, agilidad;
+    private int danio, utilidad, agilidad;
+    private String nombre;
 
     /**
      * Inicializa la clase Arma con el danio, utilidad y agilidad.
      */
-    public Arma(int danio, int utilidad, int agilidad)
+    public Arma(int danio, int utilidad, int agilidad, String nombre)
     {
         this.danio = danio;
         this.utilidad = utilidad;
         this.agilidad = agilidad;
+        this.nombre = nombre;
     }
 
     /**
-    * Esta clase recupera la utilidad del arma para aumentarle los posibles usos.
-    */
+     * Esta clase recupera la utilidad del arma para aumentarle los posibles usos.
+     */
     public abstract void reparar();
 
     public int getDanio()
@@ -53,5 +56,15 @@ public abstract class Arma
     public void setAgilidad(int agilidad)
     {
         this.agilidad = agilidad;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
     }
 }
